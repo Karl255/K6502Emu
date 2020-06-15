b2b6376f3b5c6cb077e518b97f85c161942da1a4
Karlo <49595438+Karl255@users.noreply.github.com>
2020-06-15 16:23:59 +0200

    Fixed StatusRegister unit tests
    Also removed the TODO to make it into a struct, because it should stay as a class

--- K6502/UnitTests/StatusRegisterTests.cs
+++ K6502/UnitTests/StatusRegisterTests.cs
@@ -8,86 +8,91 @@ namespace UnitTests
 		[Theory]
 		[InlineData(0)]
 		[InlineData(1)]
+		[InlineData(2)]
 		[InlineData(3)]
 		[InlineData(4)]
-		[InlineData(5)]
 		[InlineData(6)]
 		[InlineData(7)]
 		public void StatusRegisterBitSetTrueTest(byte pos)
 		{
-			var sr = new StatusRegister();
+			var sr = new StatusRegister
+			{
+				Byte = 0
+			};
 
 			switch (pos)
 			{
 				case 0:
-					sr.Negative = true;
+					sr.Carry = true;
 					break;
 				case 1:
-					sr.Overflow = true;
-					break;
-				case 3:
-					sr.Break = true;
-					break;
-				case 4:
-					sr.Decimal = true;
-					break;
-				case 5:
-					sr.Interrupt = true;
-					break;
-				case 6:
 					sr.Zero = true;
 					break;
+				case 2:
+					sr.Interrupt = true;
+					break;
+				case 3:
+					sr.Decimal = true;
+					break;
+				case 4:
+					sr.Break = true;
+					break;
+				case 6:
+					sr.Overflow = true;
+					break;
 				case 7:
-					sr.Carry = true;
+					sr.Negative = true;
 					break;
 				default:
 					break;
 			}
 
-			Assert.Equal((byte)(1 << pos) | 0b0000_0100, sr.Byte);
+			Assert.Equal((byte)(1 << pos) | 0b0010_0000, sr.Byte);
 		}
 
 		[Theory]
 		[InlineData(0)]
 		[InlineData(1)]
+		[InlineData(2)]
 		[InlineData(3)]
 		[InlineData(4)]
-		[InlineData(5)]
 		[InlineData(6)]
 		[InlineData(7)]
 		public void StatusRegisterBitSetFalseTest(byte pos)
 		{
-			var sr = new StatusRegister();
-			sr.Byte = 0B1111_1111;
+			var sr = new StatusRegister
+			{
+				Byte = 0xff
+			};
 
 			switch (pos)
 			{
 				case 0:
-					sr.Negative = false;
+					sr.Carry = false;
 					break;
 				case 1:
-					sr.Overflow = false;
-					break;
-				case 3:
-					sr.Break = false;
-					break;
-				case 4:
-					sr.Decimal = false;
-					break;
-				case 5:
-					sr.Interrupt = false;
-					break;
-				case 6:
 					sr.Zero = false;
 					break;
+				case 2:
+					sr.Interrupt = false;
+					break;
+				case 3:
+					sr.Decimal = false;
+					break;
+				case 4:
+					sr.Break = false;
+					break;
+				case 6:
+					sr.Overflow = false;
+					break;
 				case 7:
-					sr.Carry = false;
+					sr.Negative = false;
 					break;
 				default:
 					break;
 			}
 
-			Assert.Equal((byte)~(byte)(1 << pos), sr.Byte);
+			Assert.Equal((byte)(~(byte)(1 << pos) | 0B0010_0000), sr.Byte);
 		}
 
 		[Theory]
@@ -105,14 +110,14 @@ namespace UnitTests
 
 			Assert.True(pos switch
 			{
-				0 => sr.Negative,
-				1 => sr.Overflow,
+				0 => sr.Carry,
+				1 => sr.Zero,
+				2 => sr.Interrupt,
+				3 => sr.Decimal,
+				4 => sr.Break,
 
-				3 => sr.Break,
-				4 => sr.Decimal,
-				5 => sr.Interrupt,
-				6 => sr.Zero,
-				7 => sr.Carry,
+				6 => sr.Overflow,
+				7 => sr.Negative,
 				_ => true
 			});
 		}
