09e949d1d08532b9fd266d54d68e1023dcb3d7e4
Karlo <49595438+Karl255@users.noreply.github.com>
2020-06-17 19:23:09 +0200

    Implemented MirroredMemory

---
+++ K6502/MirroredMemory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace K6502Emu
+{
+	public class MirroredMemory : Memory
+	{
+		private readonly int MirrorSize;
+
+		public MirroredMemory(Range addressRange, int mirrorSize) : base(addressRange) => MirrorSize = mirrorSize;
+		public MirroredMemory(Range addressRange, byte[] data, int mirrorSize) : base(addressRange, data) => MirrorSize = mirrorSize;
+
+		public override byte this[ushort address]
+		{
+			get => memory[(address - AddressRange.Start.Value) % MirrorSize];
+			set { if (!IsReadOnly) memory[(address - AddressRange.Start.Value) % MirrorSize] = value; }
+		}
+	}
+}
